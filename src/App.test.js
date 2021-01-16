8cfa034f2a53f244adcdb0c9674e515a73d2b768
Sergei Liddell <liddell.s@pm.me>
2021-01-16 06:31:14 -0700

    Implements wrapper.debug to view component

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,16 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+// setup file
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
 
 describe('Counter Testing Suite', () => {
   test('Renders title of counter', () => {
+    const wrapper = shallow(<App />)
+    console.log(wrapper.debug());
     render(<App />);
     const linkElement = screen.getByText('This is counter app');
     expect(linkElement).toBeInTheDocument();
