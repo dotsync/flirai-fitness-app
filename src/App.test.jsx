89f9696be97c38f3d927694c76cfcacd76c2438c
Sergei Liddell <liddell.s@pm.me>
2021-01-16 07:06:10 -0700

    Enforces style guide for testing sweet

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,19 +1,21 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+/* eslint-disable no-undef */
+// import { render, screen } from '@testing-library/react';
+import React from 'react';
 
 // setup file
 import { configure, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import App from './App';
 
 configure({ adapter: new Adapter() });
 
 describe('Counter Testing Suite', () => {
   it('renders title of counter', () => {
-    const wrapper = shallow(<App />)
-    expect(wrapper.find('h1').text()).toContain('This is a counter app')
+    const wrapper = shallow(<App />);
+    expect(wrapper.find('h1').text()).toContain('This is a counter app');
   });
   it('renders a counter button', () => {
-    const wrapper = shallow(<App />)
-    expect(wrapper.find('#increment-btn').text()).toBe('incrememnt')
-  })
+    const wrapper = shallow(<App />);
+    expect(wrapper.find('#increment-btn').text()).toBe('incrememnt');
+  });
 });
