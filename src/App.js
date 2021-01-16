28e8db8505bf234630b31f0cd7edcbf5c0c6ac4b
Sergei Liddell <liddell.s@pm.me>
2021-01-16 05:55:09 -0700

    Creates a plan for routing

--- src/App.js
+++ src/App.js
@@ -2,18 +2,14 @@ import './App.css';
 
 function App() {
 
-  // get the url
-// var url = window.location.href;
-// //getting the access token from url
-// var access_token = url.split("#")[1].split("=")[1].split("&")[0];
-// // get the userid
-// var userId = url.split("#")[1].split("=")[2].split("&")[0];
-// console.log(access_token);
-// console.log(userId);
-
   return (
     <div>
-      <a href="https://www.fitbit.com/oauth2/authorize?response_type=code&client_id=22C6PH&redirect_uri=https%3A%2F%2Flocalhost:3000&scope=activity%20heartrate%20location%20nutrition%20profile%20settings%20sleep%20social%20weight&expires_in=604800">login to fitbit</a>
+      {/* Header
+        Routing/pages:
+          1.
+          2.
+          3.
+      */}
     </div>
   );
 }
