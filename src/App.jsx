ebc89241faad4ad693576f5498a44eb4d18da21b
Sergei Liddell <liddell.s@pm.me>
2021-01-17 14:57:18 -0700

    Implements basic routing for Login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import './App.css';
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
+import Login from './pages/Login/Login';
 
 function App() {
   return (
     <div className="app">
       <Navbar />
+      <Switch>
+        <Route path="/" component={Login} />
+      </Switch>
     </div>
   );
 }
