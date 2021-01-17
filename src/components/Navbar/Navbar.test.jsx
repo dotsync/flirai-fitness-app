de73ffb004d65c91f902936281859ae7a753f4bc
Sergei Liddell <liddell.s@pm.me>
2021-01-17 13:26:01 -0700

    Implements test suite for Navbar options and links

--- src/components/Navbar/Navbar.test.jsx
+++ src/components/Navbar/Navbar.test.jsx
@@ -11,7 +11,19 @@ describe('Navbar Test Suite', () => {
     wrapper = shallow(<Navbar />);
   });
 
-  test('should have a title', () => {
+  test('should have a navbar', () => {
     expect(wrapper.find('.navbar').text()).toContain('navbar');
+  });
+  test('should have a logo', () => {
+    expect(wrapper.find('.logo').text()).toContain('flirai');
+  });
+  test('should have options container for links and search', () => {
+    expect(wrapper.find('.navbar-options').text()).toContain('')
+  });
+  test('should have a player score', () => {
+    expect(wrapper.find('.navbar-options').text()).toContain('player score');
+  });
+  test('renders an initial value of state in a div', () => {
+    expect(wrapper.find('.player-score-value').text()).toBe('0');
   });
 });
