b83dd83f9aa8482273c8a6f3034d6d0aa8988f43
Sergei Liddell <liddell.s@pm.me>
2021-01-16 17:16:14 -0700

    Creates failing test to find Navbar id tag

---
+++ src/components/Navbar/Navbar.test.jsx
@@ -0,0 +1,17 @@
+/* eslint-disable no-undef */
+import React from 'react';
+
+import { shallow } from 'enzyme';
+import Navbar from './Navbar';
+
+describe('Navbar Test Suite', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<Navbar />);
+  });
+
+  test('should have a title', () => {
+    expect(wrapper.find('#navbar').text()).toContain('navbar');
+  });
+});
