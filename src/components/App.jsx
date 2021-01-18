4760117ffc158d9a2c1dfe51c1e526b3d0cdbbb8
Sergei Liddell <liddell.s@pm.me>
2021-01-17 20:07:20 -0700

    Implements authorization for Signup component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,16 +2,19 @@ import './App.css';
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Navbar from './Navbar/Navbar';
-import Login from '../pages/Login/Login';
+import { AuthProvider } from '../contexts/AuthContext';
+import Signup from './Auth/Signup/Signup';
 
 function App() {
   return (
-    <div className="app">
-      <Navbar />
-      <Switch>
-        <Route path="/" component={Login} />
-      </Switch>
-    </div>
+    <AuthProvider>
+      <div className="app">
+        <Navbar />
+        <Switch>
+          <Route path="/signup" component={Signup} />
+        </Switch>
+      </div>
+    </AuthProvider>
   );
 }
 
--- src/components/Auth/Signup/Signup.jsx
+++ src/components/Auth/Signup/Signup.jsx
@@ -1,50 +1,77 @@
-import React, { useRef } from 'react';
+/* eslint-disable consistent-return */
+/* eslint-disable no-unused-vars */
+import React, { useRef, useState } from 'react';
 import './Signup.css';
+import { useAuth } from '../../../contexts/AuthContext';
 
 export default function Signup() {
   const emailRef = useRef();
   const passwordRef = useRef();
   const passwordConfirmRef = useRef();
+  const { signup, currentUser } = useAuth();
+  const [error, setError] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    if (passwordRef.current.value !== passwordConfirmRef.current.value) {
+      return setError('Passwords do not match');
+    }
+
+    try {
+      setError('');
+      setLoading(true);
+      await signup(emailRef.current.value, passwordRef.current.value);
+    } catch {
+      setError('Failed to create an account');
+    }
+    setLoading(false);
+  }
+
   return (
     <div className="signup">
-      <div className="signup-form">
-        <h2>Sign Up</h2>
-        <form>
-          <label htmlFor="email">
-            email
-            <input
-              name="email"
-              type="email"
-              id="email"
-              required
-              ref={emailRef}
-            />
-          </label>
-          <label htmlFor="password">
-            Password
-            <input
-              name="password"
-              type="password"
-              id="password"
-              required
-              ref={passwordRef}
-            />
-          </label>
-          <label htmlFor="password-confirm">
-            Password Confirmation
-            <input
-              name="password-confirm"
-              type="password"
-              id="password-confirm"
-              required
-              ref={passwordConfirmRef}
-            />
-          </label>
-          <button className="signup-submit" type="submit">Submit</button>
-        </form>
-      </div>
-      <div className="signup-login">
-        Already have an account? Log in
+      <div className="signup-container">
+        <div className="signup-form">
+          <h2>Sign Up</h2>
+
+          <form onSubmit={handleSubmit}>
+            <label htmlFor="email">
+              email
+              <input
+                name="email"
+                type="email"
+                id="email"
+                required
+                ref={emailRef}
+              />
+            </label>
+            <label htmlFor="password">
+              Password
+              <input
+                name="password"
+                type="password"
+                id="password"
+                required
+                ref={passwordRef}
+              />
+            </label>
+            <label htmlFor="password-confirm">
+              Password Confirmation
+              <input
+                name="password-confirm"
+                type="password"
+                id="password-confirm"
+                required
+                ref={passwordConfirmRef}
+              />
+            </label>
+            <button className="signup-submit" type="submit" disabled={loading}>Submit</button>
+          </form>
+        </div>
+        <div className="signup-login">
+          Already have an account? Log in
+        </div>
       </div>
     </div>
   );
