0de0e85a337475babbf030033b301c7875abc431
Sergei Liddell <liddell.s@pm.me>
2021-01-18 06:58:45 -0700

    Moves routing from index to App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,22 @@
 import './App.css';
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './Navbar/Navbar';
 import { AuthProvider } from '../contexts/AuthContext';
 import Signup from '../pages/Auth/Signup/Signup';
 
 function App() {
   return (
-    <AuthProvider>
-      <div className="app">
-        <Navbar />
-        <Switch>
-          <Route path="/signup" component={Signup} />
-        </Switch>
-      </div>
-    </AuthProvider>
+    <div className="app">
+      <Router>
+        <AuthProvider>
+          <Navbar />
+          <Switch>
+            <Route path="/signup" component={Signup} />
+          </Switch>
+        </AuthProvider>
+      </Router>
+    </div>
   );
 }
 
