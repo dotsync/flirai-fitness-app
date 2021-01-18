150c035ed28e127826f1cc0403a349974b29209b
Sergei Liddell <liddell.s@pm.me>
2021-01-18 10:53:00 -0700

    Creates basic ExerciseCard component

---
+++ src/components/ExerciseCard/ExerciseCard.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function ExerciseCard() {
+  return (
+    <div>
+      <h3>Exercise Card</h3>
+    </div>
+  );
+}
--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import ExerciseCard from '../../components/ExerciseCard/ExerciseCard';
 
 export default function Homepage() {
   return (
     <div>
       <h1>HOME PAGE</h1>
+      <ExerciseCard />
     </div>
   );
 }
