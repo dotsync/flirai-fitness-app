77d0e4a56d782f3995e2fac39147b3d86e05ada4
Sergei Liddell <liddell.s@pm.me>
2021-01-19 06:08:08 -0700

    Completes basics test suit for FlashCard

--- src/components/FlashCard/FlashCard.test.jsx
+++ src/components/FlashCard/FlashCard.test.jsx
@@ -25,7 +25,7 @@ describe('Flashcard Component Test Suite', () => {
       expect(wrapper.text()).toBe('loading');
     });
   });
-  // setup
+  // setup props
   const setUp = (props = {}) => {
     // passes a dummy to myFlashcards prop
     const component = shallow(<Flashcard myFlashcards={testCard} />);
@@ -51,36 +51,17 @@ describe('Flashcard Component Test Suite', () => {
       expect(wrapper.text()).toBe(`${testCard.question}`);
     });
   });
-  // Checks myFlashcard has buttons
+  // Checks flashcard has buttons
   describe('It has buttons', () => {
+    // delete
     test('should have a button to delete the flashcard', () => {
-      // console.log('SHOW', component.debug())
       wrapper = component.find('#flashcard-delete-btn');
       expect(wrapper.text()).toBe('delete');
     });
+    // reveal answer
     test('Flashcard renders reveal answer button', () => {
       wrapper = component.find('#flashcard-answer-btn');
       expect(wrapper.length).toBe(1);
     });
   });
-// API
-// setup(mount?)
-// describe('It renders a flashcard using an API', () => {
-//   // let cardFromAPI
-//   test('myFlashcard renders a category', () => {
-//     const component = shallow(<Flashcard myFlashcards={cardFromAPI} />)
-//     const wrapper = component.find('.makeStyles-category-8')
-//     // exact match
-//     expect(wrapper.text()).toBe('match the api')
-//   });
-//   test('myFlashcard renders a subject', () => {
-//     const component = shallow(<Flashcard myFlashcards={cardFromAPI} />)
-//     const wrapper = component.find('.makeStyles-subject-9')
-//     expect(wrapper.text()).toBe('match the api')
-//   });
-//   test('myFlashcard renders a question', () => {
-//     const component = shallow(<Flashcard myFlashcards={cardFromAPI} />)
-//     const wrapper = component.find('.makeStyles-question-10')
-//     expect(wrapper.text()).toBe('match the api')
-//   });
 });
