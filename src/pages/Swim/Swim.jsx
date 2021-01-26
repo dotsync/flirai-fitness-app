5ff4f62c9740f53fb4018a379eaaf9c6f3460fe7
Sergei Liddell <liddell.s@pm.me>
2021-01-26 06:51:38 -0700

    Drastically improves dummy card

--- src/pages/Swim/Swim.jsx
+++ src/pages/Swim/Swim.jsx
@@ -3,11 +3,34 @@ import ExerciseCard from '../../components/ExerciseCard/ExerciseCard';
 
 export default function Swim() {
   const DUMMY_EXERCISE_CARD = {
+    // reducer(accumulator, currentValue){ accumulator + currentValue;},
     title: 'Endurance Swim',
     subject: 'Swim',
     description: 'A continuos easy-paced swim to develop your endurance',
+    totalDistance: 1300,
+    unit: 'meters',
+    warmup: {
+      type: ['freestyle'],
+      amount: 100,
+    },
+    drills: {
+      type: ['3stroke-6kick, fist, catch-up, zipper'],
+      amount: 50,
+    },
+    mainset: {
+      type: 'Endurance Swim',
+      repetitions: [100, 200, 300, 400],
+      sets: 4,
+      perceivedExertion: 4,
+      rest: 60,
+
+    },
+    cooldown: {
+      type: ['freestyle'],
+      amount: 100,
+    },
     time: 90,
-    Complete: true,
+    completed: false,
   };
   return (
     <div>
