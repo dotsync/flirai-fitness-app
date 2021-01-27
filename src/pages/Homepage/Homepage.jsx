8077009697d1ef9042c6ed5944eb30e3050689a1
Sergei Liddell <liddell.s@pm.me>
2021-01-27 16:25:26 -0700

    Creates starry VR envirmoent

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -4,13 +4,8 @@ import './Homepage.css';
 export default function Homepage() {
   return (
     <div className="home-page">
-      <a-scene background="color: midnightblue">
-        <a-box
-          color="#F5C85D"
-          position="0 0 -5"
-          rotation="33 45 0"
-          scale="2 1 2"
-        />
+      <a-scene>
+        <a-entity environment="preset: starry" />
       </a-scene>
     </div>
   );
