e87016624f7928c39a10ce91ca078cf6733b93f6
Sergei Liddell <liddell.s@pm.me>
2021-01-28 05:34:44 -0700

    Implements a sky and ground for StrandedVR

--- src/pages/StrandedVR/StrandedVR.jsx
+++ src/pages/StrandedVR/StrandedVR.jsx
@@ -1,15 +1,27 @@
+/* eslint-disable react/self-closing-comp */
 import React from 'react';
 
 export default function SolarSystem() {
   return (
     <div>
       <a-scene>
+        <a-assets>
+          <img alt="groundTexture" id="groundTexture" src="https://cdn.aframe.io/a-painter/images/floor.jpg" />
+          <img alt="skyTexture" id="skyTexture" src="https://cdn.aframe.io/a-painter/images/sky.jpg" />
+        </a-assets>
         <a-cylinder
           id="ground"
-          src="https://cdn.aframe.io/a-painter/images/floor.jpg"
+          src="#groundTexture"
           radius="32"
           height="0.1"
         />
+        <a-sky
+          id="background"
+          src="#skyTexture"
+          theta-length="90"
+          radius="30"
+        >
+        </a-sky>
       </a-scene>
     </div>
   );
