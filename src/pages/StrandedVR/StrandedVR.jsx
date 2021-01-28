1da9f1433e9aad36491c5466d689eb06a6b441d9
Sergei Liddell <liddell.s@pm.me>
2021-01-28 05:09:29 -0700

    Creates a scene for StrandedVR game

--- src/pages/StrandedVR/StrandedVR.jsx
+++ src/pages/StrandedVR/StrandedVR.jsx
@@ -3,7 +3,14 @@ import React from 'react';
 export default function SolarSystem() {
   return (
     <div>
-      s
+      <a-scene>
+        <a-cylinder
+          id="ground"
+          src="https://cdn.aframe.io/a-painter/images/floor.jpg"
+          radius="32"
+          height="0.1"
+        />
+      </a-scene>
     </div>
   );
 }
