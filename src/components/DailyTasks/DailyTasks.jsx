8c1274a0816a2da678bcace1fa0206d23490ec23
Sergei Liddell <liddell.s@pm.me>
2021-01-28 04:55:34 -0700

    Removes hardcoded ExerciseCards

--- src/components/DailyTasks/DailyTasks.jsx
+++ src/components/DailyTasks/DailyTasks.jsx
@@ -1,14 +1,10 @@
 import React from 'react';
 import './DailyTasks.css';
-import ExerciseCard from '../ExerciseCard/ExerciseCard';
 
 export default function DailyTask() {
   return (
     <div className="daily-tasks-row">
-      <ExerciseCard />
-      <ExerciseCard />
-      <ExerciseCard />
-      <ExerciseCard />
+      hello from daily tasks
     </div>
   );
 }
